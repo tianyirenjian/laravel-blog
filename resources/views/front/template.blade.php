40c36fde2baef1a296170a624df7fb83ddb9605f
goenitz <179063828@qq.com>
2015-12-02 14:27:05 +0800

    添加手机适配

--- resources/views/front/template.blade.php
+++ resources/views/front/template.blade.php
@@ -10,6 +10,8 @@
         \/            \/     \/               \/                \/      \/ \/ 
 -->
 <meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 @yield('title')
 <link rel="stylesheet" type="text/css" href="{{ elixir('css/app.css') }}">
 @yield('style')
