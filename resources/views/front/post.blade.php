a454b673d8421b3a2ed27eb0d5e9b36c591449af
goenitz <179063828@qq.com>
2015-12-02 14:00:15 +0800

    更改评论为多说

--- resources/views/front/post.blade.php
+++ resources/views/front/post.blade.php
@@ -28,10 +28,22 @@
                             @if(!$article->password || (\Session::get('passed_'.$article->id)))
                             {!! Markdown::convertToHtml($article->content) !!}
                             <div style="margin-bottom: 10px;padding-bottom: 15px;"></div>
-                            <!-- UY BEGIN -->
-                            <div id="uyan_frame"></div>
-                            <script type="text/javascript" src="http://v2.uyan.cc/code/uyan.js?uid=1679714"></script>
-                            <!-- UY END -->
+                            <!-- 多说评论框 start -->
+                                <div class="ds-thread" data-thread-key="{{ $article->id }}" data-title="{{ $article->title }}" data-url="http://www.goenitz.xyz/post/{{ $article->slug }}"></div>
+                            <!-- 多说评论框 end -->
+                            <!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+                            <script type="text/javascript">
+                            var duoshuoQuery = {short_name:"goenitzxyz"};
+                                (function() {
+                                    var ds = document.createElement('script');
+                                    ds.type = 'text/javascript';ds.async = true;
+                                    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+                                    ds.charset = 'UTF-8';
+                                    (document.getElementsByTagName('head')[0] 
+                                     || document.getElementsByTagName('body')[0]).appendChild(ds);
+                                })();
+                                </script>
+                            <!-- 多说公共JS代码 end -->
                             @else
                             <div class="row">
                                 <div class="col-md-6">
